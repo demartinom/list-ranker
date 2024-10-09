807e893f489fedeb3216fa158f087b003073055c
Matt DeMartino <matt.s.demartino@gmail.com>
2024-10-09 14:43:40 -0400

    Create flag for receiving list to rank

--- main.go
+++ main.go
@@ -1,5 +1,13 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+)
 
+func main() {
+	list := flag.String("list", "cities.csv", "list to use for ranking")
+	flag.Parse()
+
+	fmt.Println(*list)
 }
