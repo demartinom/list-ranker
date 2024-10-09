8e96f59dd06f3eea73893c9665e652d84660395c
Matt DeMartino <matt.s.demartino@gmail.com>
2024-10-09 14:58:30 -0400

    Read information from csv file

--- main.go
+++ main.go
@@ -1,13 +1,32 @@
 package main
 
 import (
+	"encoding/csv"
 	"flag"
 	"fmt"
+	"log"
+	"os"
 )
 
 func main() {
 	list := flag.String("list", "cities.csv", "list to use for ranking")
 	flag.Parse()
 
-	fmt.Println(*list)
+	file, err := os.Open(*list)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+
+	records, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, v := range records {
+		fmt.Println(v[0])
+	}
 }
