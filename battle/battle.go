b56e98e6f1eba9c53ec87bfa6854279877f509ea
Matt DeMartino <matt.s.demartino@gmail.com>
2024-10-10 13:22:17 -0400

    Create function to randomly choose two items

--- battle/battle.go
+++ battle/battle.go
@@ -1,5 +1,7 @@
 package battle
 
+import "math/rand"
+
 type Item struct {
 	Name  string
 	Score int
@@ -13,6 +15,21 @@ func (item *Item) Lose() {
 	item.Score--
 }
 
-func Battle([]Item) {
+func Battle(list []Item) {
+	battlers := chooseBattlers(list)
+}
 
+func chooseBattlers(list []Item) []Item {
+	fighterOneIndex := rand.Intn(len(list))
+	fighterTwoIndex := rand.Intn(len(list))
+
+	for fighterOneIndex == fighterTwoIndex {
+		fighterTwoIndex = rand.Intn(len(list))
+	}
+
+	fighterOne := list[fighterOneIndex]
+	fighterTwo := list[fighterTwoIndex]
+
+	combatants := []Item{fighterOne, fighterTwo}
+	return combatants
 }
