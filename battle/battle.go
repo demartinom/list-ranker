6325c7302beaa977fb4b828d065c9f24a4b10c9c
Matt DeMartino <matt.s.demartino@gmail.com>
2024-10-11 13:07:58 -0400

    Remove item from options list if score is too low

--- battle/battle.go
+++ battle/battle.go
@@ -14,13 +14,16 @@ func (item *Item) Win() {
 	item.Score++
 }
 
-func (item *Item) Lose() {
+func (item *Item) Lose(list *[]Item, index int) {
 	item.Score--
+	if item.Score <= -2 {
+		RemoveLoser(list, index)
+	}
 }
 
 func Battle(list *[]Item) {
 	for len(*list) > 1 {
-		battlers := chooseBattlers(*list)
+		battlers, indexes := chooseBattlers(*list)
 		var selection string
 
 		fmt.Println("Choose which item you prefer:")
@@ -34,11 +37,13 @@ func Battle(list *[]Item) {
 			switch selection {
 			case "1":
 				battlers[0].Win()
-				battlers[1].Lose()
+				battlers[1].Lose(list, indexes[1])
+				fmt.Println(list)
 				break selectloop
 			case "2":
 				battlers[1].Win()
-				battlers[0].Lose()
+				battlers[0].Lose(list, indexes[0])
+				fmt.Println(list)
 				break selectloop
 			default:
 				fmt.Println("Invalid input")
@@ -49,7 +54,7 @@ func Battle(list *[]Item) {
 	}
 }
 
-func chooseBattlers(list []Item) []*Item {
+func chooseBattlers(list []Item) ([]*Item, []int) {
 	fighterOneIndex := rand.Intn(len(list))
 	fighterTwoIndex := rand.Intn(len(list))
 
@@ -61,5 +66,10 @@ func chooseBattlers(list []Item) []*Item {
 	fighterTwo := &list[fighterTwoIndex]
 
 	combatants := []*Item{fighterOne, fighterTwo}
-	return combatants
+	indexes := []int{fighterOneIndex, fighterTwoIndex}
+	return combatants, indexes
+}
+
+func RemoveLoser(list *[]Item, index int) {
+	*list = append((*list)[:index], (*list)[index+1:]...)
 }
