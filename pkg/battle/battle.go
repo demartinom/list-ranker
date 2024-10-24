54558772e29dee412fe8a31fc148b2d5baaad696
Matt DeMartino <matt.s.demartino@gmail.com>
2024-10-24 15:12:32 -0400

    Move Item methods to types file

--- pkg/battle/battle.go
+++ pkg/battle/battle.go
@@ -1,27 +1,10 @@
 package battle
 
+import "fmt"
+
 func Battle(list *[]Item, input input, output output) {
 
 }
-
-// // Increment winner of battles score. Capped at 5 to increase game speed.
-// func (item *Item) Win() {
-// 	if item.Score < 5 {
-// 		item.Score++
-// 	}
-// }
-
-// // Decrement loser of battles score.
-// // If score drops below -2 they will be removed from the game
-// func (item *Item) Lose(list *[]Item, index int, results *[]string) {
-// 	item.Score--
-// 	if len(*list) == 2 {
-// 		RemoveLoser(list, index, results)
-// 	}
-// 	if item.Score <= -2 {
-// 		RemoveLoser(list, index, results)
-// 	}
-// }
 
 // func Battle(list *[]Item) {
 // 	var results []string
@@ -77,13 +60,13 @@ func Battle(list *[]Item, input input, output output) {
 // 	return combatants, indexes
 // }
 
-// // When score threshold met, item is removed from pool of items
-// // and added to final ranking
-// func RemoveLoser(list *[]Item, index int, results *[]string) {
-// 	placement := fmt.Sprintf("%d: %s", len(*list), (*list)[index].Name)
-// 	*results = append(*results, placement)
-// 	*list = append((*list)[:index], (*list)[index+1:]...)
-// }
+// When score threshold met, item is removed from pool of items
+// and added to final ranking
+func RemoveLoser(list *[]Item, index int, results *[]string) {
+	placement := fmt.Sprintf("%d: %s", len(*list), (*list)[index].Name)
+	*results = append(*results, placement)
+	*list = append((*list)[:index], (*list)[index+1:]...)
+}
 
 // // Prints out the final item rankings
 // func endResult(results []string, list *[]Item) {
