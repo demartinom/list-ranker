8588d4ceb7d9d7f3ecbd1fa916d90f5be00b1dbb
Matt DeMartino <matt.s.demartino@gmail.com>
2025-02-10 12:52:29 -0500

    Create function to handle routing

--- server/pkg/routes/routes.go
+++ server/pkg/routes/routes.go
@@ -1,7 +1,10 @@
 package routes
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/demartinom/list-ranker-v2/pkg/handlers"
+	"github.com/gin-gonic/gin"
+)
 
 func SetupRoutes(router *gin.Engine)  {
-	
+	router.GET("/api/premades", handlers.SendPremade)
 }
