9d2489d2912e2b35c151276e7d527af35b567760
Matt DeMartino <matt.s.demartino@gmail.com>
2025-02-10 12:54:24 -0500

    Add route handler to router

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -3,11 +3,14 @@ package main
 import (
 	"log"
 
+	"github.com/demartinom/list-ranker-v2/pkg/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 router := gin.Default()
+
+routes.SetupRoutes(router)
 
 log.Println("Server running on http://localhost:8080")
 router.Run(":8080")
