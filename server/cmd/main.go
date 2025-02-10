538cd06806788607035e94b7a274378865dc6b1b
Matt DeMartino <matt.s.demartino@gmail.com>
2025-02-10 12:24:50 -0500

    Init web server

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -1,5 +1,14 @@
 package main
 
-func main() {
+import (
+	"log"
 
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+router := gin.Default()
+
+log.Println("Server running on http://localhost:8080")
+router.Run(":8080")
 }
