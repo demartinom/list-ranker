8783375259a833bd58e1508ca9b96dc5d0cfcc33
Matt DeMartino <matt.s.demartino@gmail.com>
2025-02-14 14:23:26 -0500

    Install gin cors to allow api requests to backend

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -2,13 +2,24 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/demartinom/list-ranker-v2/pkg/routes"
+	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 )
 
 func main() {
 router := gin.Default()
+
+router.Use(cors.New(cors.Config{
+	AllowOrigins:     []string{"http://localhost:5173"}, // Allow requests from your frontend
+	AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+	AllowHeaders:     []string{"Content-Type", "Authorization"},
+	ExposeHeaders:    []string{"Content-Length"},
+	AllowCredentials: true,
+	MaxAge:           12 * time.Hour,
+}))
 
 routes.SetupRoutes(router)
 
