3db902ff7458f82cf41016eddb65d822635b7b08
Matt DeMartino <matt.s.demartino@gmail.com>
2025-02-14 14:23:11 -0500

    Use api call to receive premade lists and store in state

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,3 +1,16 @@
+import { useEffect, useState } from "react";
+import { getPremades } from "./api/api";
+
 export default function App() {
+  const [premadeLists, setPremadeLists] = useState([]);
+
+  useEffect(() => {
+    const fetchPremades = async () => {
+      const data = await getPremades();
+      if (data) setPremadeLists(data);
+    };
+    fetchPremades();
+  }, []);
+  console.log(premadeLists);
   return <div>App</div>;
 }
