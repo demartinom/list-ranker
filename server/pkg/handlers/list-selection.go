db56dc5103ca18a881d75e202c580b160c851cad
Matt DeMartino <matt.s.demartino@gmail.com>
2025-02-14 17:05:32 -0500

    Create struct to bind api post to

--- server/pkg/handlers/list-selection.go
+++ server/pkg/handlers/list-selection.go
@@ -1,14 +1,31 @@
 package handlers
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/demartinom/list-ranker-v2/pkg/models"
 	"github.com/gin-gonic/gin"
 )
 
+type Choice struct {
+	Selection string
+}
+
+
 func SendPremade(c *gin.Context){
 	premadeLists := models.PremadeList()
 	
 	c.JSON(http.StatusOK, gin.H{"premades":premadeLists})
+}
+
+func ReceiveChoice(c *gin.Context) {
+	var req Choice
+
+	// Bind JSON to struct
+	if err := c.BindJSON(&req); err != nil {
+		return
+	}
+
+	fmt.Println("Received choice:", req.Selection)
 }
