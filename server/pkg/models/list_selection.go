5f6a366bc7fd7e2d70e20b1ae51773d91f9bc604
Matt DeMartino <matt.s.demartino@gmail.com>
2025-03-05 12:43:56 -0500

    Move read csv function

--- server/pkg/models/list_selection.go
+++ server/pkg/models/list_selection.go
@@ -1,8 +1,11 @@
 package models
 
 import (
+	"encoding/csv"
+	"fmt"
 	"io/fs"
 	"log"
+	"os"
 	"path/filepath"
 )
 
@@ -28,4 +31,30 @@ func getFileNames() []string {
 		log.Fatal(err)
 	}
 	return files
+}
+
+func ReadCSV(fileName string) []*Item {
+	filePath := fmt.Sprintf("game-data/%s.csv", fileName)
+	file, err := os.Open(filePath)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer file.Close()
+	reader := csv.NewReader(file)
+	//Skip over header line
+	reader.Read()
+
+	listItems, err := reader.ReadAll()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	var itemsList []*Item
+
+	for _, itemInput := range listItems {
+		itemsList = append(itemsList, &Item{Name: itemInput[0], Score: 0})
+	}
+
+	return itemsList
 }
