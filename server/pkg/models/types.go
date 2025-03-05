cd0cfd0964efbab1636f32f60ed444ae7e5f39d1
Matt DeMartino <matt.s.demartino@gmail.com>
2025-03-05 12:42:44 -0500

    Add documentation

--- server/pkg/models/types.go
+++ server/pkg/models/types.go
@@ -1,13 +1,16 @@
 package models
 
+// Create global state for list selected for battle
 type ListState struct {
 	BattleList []*Item
 }
 
+// Sets the list for the current game to the user selection
 func (l *ListState) SetList(list []*Item) {
 	l.BattleList = list
 }
 
+// Struct for receiving messages from the frontend
 type Choice struct {
 	Selection string `json:"selection"`
 }
