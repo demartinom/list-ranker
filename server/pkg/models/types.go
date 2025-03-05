c8d3233d54fe9001e910a59e324a88362859c9a6
Matt DeMartino <matt.s.demartino@gmail.com>
2025-03-05 12:37:19 -0500

    Create global state for battle list and method to init list

--- server/pkg/models/types.go
+++ server/pkg/models/types.go
@@ -1,5 +1,13 @@
 package models
 
+type ListState struct {
+	BattleList []*Item
+}
+
+func (l *ListState) SetList(list []*Item) {
+	l.BattleList = list
+}
+
 type Choice struct {
 	Selection string `json:"selection"`
 }
