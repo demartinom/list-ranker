c6c898eb5b0ec9c67cfa706f0457c07ce3997657
Matt DeMartino <matt.s.demartino@gmail.com>
2025-03-05 13:06:49 -0500

    Create methods for setting current fighters and indexes

--- server/pkg/models/types.go
+++ server/pkg/models/types.go
@@ -2,12 +2,22 @@ package models
 
 // Create global state for list selected for battle
 type ListState struct {
-	BattleList []*Item
+	BattleList        []*Item
+	CurrentCombatants []*Item
+	CurrentIndexes    []int
 }
 
 // Sets the list for the current game to the user selection
 func (l *ListState) SetList(list []*Item) {
 	l.BattleList = list
+}
+
+func (l *ListState) SetCurrentFighters(fighters []*Item) {
+	l.CurrentCombatants = fighters
+}
+
+func (l *ListState) SetCurrentIndexes(indexes []int) {
+	l.CurrentIndexes = indexes
 }
 
 // Struct for receiving messages from the frontend
