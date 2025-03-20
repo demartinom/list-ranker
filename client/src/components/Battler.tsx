18915e53ba2747af65fb15cbd1bec6f21b12c226
Matt DeMartino <matt.s.demartino@gmail.com>
2025-03-20 15:31:31 -0400

    Update prop names

--- client/src/components/Battler.tsx
+++ client/src/components/Battler.tsx
@@ -1,14 +1,20 @@
 import { Card, CardContent } from "@/components/ui/card";
 
-interface ListChoiceProps {
-  name: string;
+interface BattlerProps {
+  battlerName: string;
   winner: () => Promise<void>;
 }
 
-export default function Battler({ name = "Item", winner }:ListChoiceProps) {
+export default function Battler({
+  battlerName = "Item",
+  winner,
+}: BattlerProps) {
   return (
-    <Card className="m-2.5 h-60 w-2xl justify-center bg-sky-200 text-center shadow-sky-200 hover:cursor-pointer" onClick={winner}>
-      <CardContent className="text-4xl text-sky-900">{name}</CardContent>
+    <Card
+      className="m-2.5 h-60 w-2xl justify-center bg-sky-200 text-center shadow-sky-200 hover:cursor-pointer"
+      onClick={winner}
+    >
+      <CardContent className="text-4xl text-sky-900">{battlerName}</CardContent>
     </Card>
   );
 }
