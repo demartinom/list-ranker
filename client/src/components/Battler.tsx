82526217c2f57d3cc1413e90631fc4ca34b540a6
Matt DeMartino <matt.s.demartino@gmail.com>
2025-03-20 15:18:03 -0400

    Add pointer cursor on hover

--- client/src/components/Battler.tsx
+++ client/src/components/Battler.tsx
@@ -2,7 +2,7 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export default function Battler({ name = "Item" }) {
   return (
-    <Card className="m-2.5 h-60 w-2xl justify-center bg-sky-200 text-center shadow-sky-200">
+    <Card className="m-2.5 h-60 w-2xl justify-center bg-sky-200 text-center shadow-sky-200 hover:cursor-pointer">
       <CardContent className="text-4xl text-sky-900">{name}</CardContent>
     </Card>
   );
