b858f3b9e801a3c6b143d28645fd7a34feaf091d
Matt DeMartino <matt.s.demartino@gmail.com>
2025-03-24 12:24:58 -0400

    Styling

--- client/src/components/Battler.tsx
+++ client/src/components/Battler.tsx
@@ -11,7 +11,7 @@ export default function Battler({
 }: BattlerProps) {
   return (
     <Card
-      className="m-2.5 h-60 w-2xl cursor-pointer justify-center bg-sky-200 text-center shadow-sky-200 transition-colors duration-200 hover:bg-sky-400"
+      className="h-60 w-2xl cursor-pointer justify-center bg-sky-200 text-center shadow-sky-200 transition-colors duration-200 hover:bg-sky-400"
       onClick={winner}
     >
       <CardContent className="text-4xl text-sky-950">{battlerName}</CardContent>
