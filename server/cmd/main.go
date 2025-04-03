08b5e33db3a37c73b84d09056912a74beb372de6
Matt DeMartino <matt.s.demartino@gmail.com>
2025-04-03 11:20:04 -0400

    Ready server for fly.io deployment

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -13,7 +13,7 @@ func main() {
 	router := gin.Default()
 
 	router.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:5173"}, // Allow requests from your frontend
+		AllowOrigins:     []string{"*"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
