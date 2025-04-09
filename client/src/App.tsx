c7c3e46fb2562396e2cf6df2c13e5af8e3b22a31
Matt DeMartino <matt.s.demartino@gmail.com>
2025-04-09 12:45:02 -0400

    Update responsiveness

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -55,39 +55,46 @@ export default function App() {
 
   // Map final rankings to list with rank number
   const rankingList = finalRanking.map((item: string, index: number) => (
-    <p key={index} className="text-3xl">
+    <p key={index} className="text-2xl md:text-3xl">
       {index + 1}: {item}
     </p>
   ));
 
   return (
-    <main className="m-auto min-h-screen w-5/6 max-w-4xl px-4">
-      <div className="mt-8 flex flex-col items-center">
-        <h1 className="text-7xl font-extrabold text-gray-700">List Ranker</h1>
+    <main className="container mx-auto min-h-screen p-4">
+      <div className="mt-8 flex flex-col items-center text-center">
+        <h1 className="text-5xl font-extrabold text-gray-700 md:text-7xl">
+          List Ranker
+        </h1>
         <Separator className="mt-5" style={{ height: "3px" }} />
       </div>
+
       {premadeLists.length > 0 && (
-        <div className="mt-10 flex flex-col items-center gap-2 p-4 sm:gap-4">
+        <div className="mt-10 flex flex-col items-center gap-4">
           <h2 className="text-3xl font-semibold text-gray-700">
             Choose a premade list to start battling
           </h2>
           <ul className="list-none">{premadeOptions}</ul>
         </div>
       )}
+
       {currentBattlers.length > 0 && (
-        <div className="mt-6 flex flex-col items-center gap-2 p-4 sm:gap-4">
-          <h2 className="text-3xl text-gray-700">
+        <div className="mt-6 flex flex-col items-center gap-4">
+          <h2 className="text-center text-3xl text-gray-700">
             Choose Which Item You Prefer
           </h2>
-          <div className="mt-10 flex justify-center gap-10">
+          <div className="mt-10 flex w-full flex-col place-items-center gap-10 lg:flex-row">
             {battleOptions}
           </div>
         </div>
       )}
+
       {finalRanking.length > 0 && (
-        <div className="mt-12 flex flex-col items-center pt-10">
-          <h2 className="text-5xl font-bold text-gray-900">Final Results</h2>
-          <div className="mt-6 flex flex-col gap-3 text-center">
+        <div className="mt-6 flex flex-col items-center">
+          <h2 className="text-4xl font-bold text-gray-900 md:text-5xl">
+            Final Results
+          </h2>
+          <div className="mt-3 flex flex-col gap-3 text-center md:mt-6">
             {finalRanking.length > 0 && rankingList}
           </div>
         </div>
