9dfb2e4d57ecf5354ff66dfa627ce808b345e9c9
Matt DeMartino <matt.s.demartino@gmail.com>
2025-05-09 12:15:36 -0400

    Add skeleton as placeholder for loading data

--- client/src/components/Battler.tsx
+++ client/src/components/Battler.tsx
@@ -1,4 +1,5 @@
 import { Card, CardContent } from "@/components/ui/card";
+import { Skeleton } from "./ui/skeleton";
 
 interface BattlerProps {
   battlerName: string;
@@ -10,11 +11,19 @@ export default function Battler({
   winner,
 }: BattlerProps) {
   return (
-    <Card
-      className="h-60 w-full cursor-pointer justify-center bg-sky-200 text-center shadow-sky-200 transition-colors duration-200 hover:bg-sky-400 sm:w-2xl"
-      onClick={winner}
-    >
-      <CardContent className="text-4xl text-sky-950">{battlerName}</CardContent>
-    </Card>
+    <>
+      {!battlerName ? (
+        <Skeleton className="h-60 w-full justify-center bg-gray-200 sm:w-2xl" />
+      ) : (
+        <Card
+          className="h-60 w-full cursor-pointer justify-center bg-sky-200 text-center shadow-sky-200 transition-colors duration-200 hover:bg-sky-400 sm:w-2xl"
+          onClick={winner}
+        >
+          <CardContent className="text-4xl text-sky-950">
+            {battlerName}
+          </CardContent>
+        </Card>
+      )}
+    </>
   );
 }
--- client/src/components/ListChoice.tsx
+++ client/src/components/ListChoice.tsx
@@ -1,4 +1,5 @@
 import { Button } from "@/components/ui/button";
+import { Skeleton } from "./ui/skeleton";
 
 interface ListProps {
   listName: string;
@@ -10,12 +11,18 @@ export default function ListChoice({
   listSelection,
 }: ListProps) {
   return (
-    <Button
-      variant={"ghost"}
-      className="m-2.5 h-22 w-fit min-w-40 cursor-pointer bg-sky-300 px-10 text-2xl text-sky-950 hover:bg-sky-500"
-      onClick={listSelection}
-    >
-      {listName}
-    </Button>
+    <>
+      {!listName ? (
+        <Skeleton className="m-2.5 h-22 w-fit min-w-50 bg-gray-200" />
+      ) : (
+        <Button
+          variant={"ghost"}
+          className="m-2.5 h-22 w-fit min-w-40 cursor-pointer bg-sky-300 px-10 text-2xl text-sky-950 hover:bg-sky-500"
+          onClick={listSelection}
+        >
+          {listName}
+        </Button>
+      )}
+    </>
   );
 }
