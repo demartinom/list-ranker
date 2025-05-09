173e7849ac454eb2a6c45988f366a79c04d58eea
Matt DeMartino <matt.s.demartino@gmail.com>
2025-05-09 12:46:03 -0400

    Revert component

--- client/src/components/Battler.tsx
+++ client/src/components/Battler.tsx
@@ -1,5 +1,4 @@
 import { Card, CardContent } from "@/components/ui/card";
-import { Skeleton } from "./ui/skeleton";
 
 interface BattlerProps {
   battlerName: string;
@@ -11,19 +10,13 @@ export default function Battler({
   winner,
 }: BattlerProps) {
   return (
-    <>
-      {!battlerName ? (
-        <Skeleton className="min-h-[10rem] w-full min-w-[16rem] bg-gray-200 sm:w-[32rem]" />
-      ) : (
-        <Card
-          className="flex min-h-[15rem] w-full min-w-[16rem] cursor-pointer items-center justify-center bg-sky-200 text-center shadow-sky-200 transition-colors duration-200 hover:bg-sky-400 sm:w-[32rem]"
-          onClick={winner}
-        >
-          <CardContent className="text-2xl text-sky-950 sm:text-4xl">
-            {battlerName}
-          </CardContent>
-        </Card>
-      )}
-    </>
+    <Card
+      className="flex min-h-[15rem] w-full min-w-[16rem] cursor-pointer items-center justify-center bg-sky-200 text-center shadow-sky-200 transition-colors duration-200 hover:bg-sky-400 sm:w-[32rem]"
+      onClick={winner}
+    >
+      <CardContent className="text-2xl text-sky-950 sm:text-4xl">
+        {battlerName}
+      </CardContent>
+    </Card>
   );
 }
