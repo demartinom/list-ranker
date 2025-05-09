b5adfd6d7336260bde8b257ab6072ea58d8430d2
Matt DeMartino <matt.s.demartino@gmail.com>
2025-05-09 13:44:57 -0400

    Remove empty div

--- client/src/components/ListChoice.tsx
+++ client/src/components/ListChoice.tsx
@@ -6,25 +6,20 @@ interface ListProps {
   listSelection: () => Promise<void>;
   loading: boolean;
 }
-
 export default function ListChoice({
   listName = "list",
   listSelection,
   loading,
 }: ListProps) {
-  return (
-    <>
-      {loading ? (
-        <Skeleton className="m-2.5 h-22 w-fit min-w-50 bg-gray-200" />
-      ) : (
-        <Button
-          variant={"ghost"}
-          className="m-2.5 h-22 w-fit min-w-40 cursor-pointer bg-sky-300 px-10 text-2xl text-sky-950 hover:bg-sky-500"
-          onClick={listSelection}
-        >
-          {listName}
-        </Button>
-      )}
-    </>
+  return loading ? (
+    <Skeleton className="m-2.5 h-22 w-fit min-w-50 bg-gray-200" />
+  ) : (
+    <Button
+      variant={"ghost"}
+      className="m-2.5 h-22 w-fit min-w-40 cursor-pointer bg-sky-300 px-10 text-2xl text-sky-950 hover:bg-sky-500"
+      onClick={listSelection}
+    >
+      {listName}
+    </Button>
   );
 }
