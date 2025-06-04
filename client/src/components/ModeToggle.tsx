f8ee01815c42d5e56c9b1502036b2f2b91e0e8d7
Matt DeMartino <matt.s.demartino@gmail.com>
2025-06-04 16:21:17 -0400

    Create mode toggle

--- client/src/components/ModeToggle.tsx
+++ client/src/components/ModeToggle.tsx
@@ -1,3 +1,28 @@
+import Sun from "@/assets/Sun";
+import Moon from "@/assets/Moon";
+import { useTheme } from "@/components/theme-provider";
+import { Button } from "./ui/button";
+
 export default function ModeToggle() {
-  return <div>ModeToggle</div>;
+  const { theme, setTheme } = useTheme();
+  const iconClasses = "size-6 md:size-10";
+
+  const buttonIcon =
+    theme == "light" ? (
+      <Sun classname={iconClasses} />
+    ) : (
+      <Moon classname={iconClasses} />
+    );
+  const changeTheme = () => {
+    setTheme(theme == "light" ? "dark" : "light");
+  };
+  return (
+    <Button
+      variant={"ghost"}
+      onClick={changeTheme}
+      className="absolute top-2 right-10 size-10 align-baseline md:size-16"
+    >
+      {buttonIcon}
+    </Button>
+  );
 }
