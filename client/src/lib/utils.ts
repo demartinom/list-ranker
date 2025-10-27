d2a65bf2f6bb1b2c9c618bceb2b188808f852cbd
Matt DeMartino <matt.s.demartino@gmail.com>
2025-10-27 16:54:44 -0400

    Create function to validate that custom list is useable

--- client/src/lib/utils.ts
+++ client/src/lib/utils.ts
@@ -8,3 +8,19 @@ export function cn(...inputs: ClassValue[]) {
 export const customListToArray = (list: string) => {
   return list.split(", ");
 };
+
+export const listValidation = (list: string): string | null => {
+  const arrayFromList = customListToArray(list);
+  if (arrayFromList.length < 4) {
+    return "Please submit a list of four items or more.";
+  }
+
+  if (new Set(arrayFromList).size !== arrayFromList.length) {
+    return "Duplicates found in list.";
+  }
+
+  if (arrayFromList.length > 40) {
+    return "Your list is too long and will take too much time to rank.";
+  }
+  return null;
+};
